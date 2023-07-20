d413be832282e8338a7d5f3b2576b4a634087998
Mateus <mateusandradedf@hotmail.com>
2023-07-20 19:01:00 -0300

    modifica handler

--- handler.py
+++ handler.py
@@ -1,52 +1,51 @@
-from rossmann.Rossmann import Rossmann
-import pickle
-from flask               import Flask, request, Response
-import pandas as pd 
 import os
+import pickle
+import requests
+import pandas as pd
+from flask             import Flask, request, Response
+from rossmann.Rossmann import Rossmann
 
 
-#loading data
+#loading model
 model = pickle.load(open('model/xgb_result.pkl', 'rb'))
+model = model['model_xgb_tuned']
 
-
+#inicio API
 app = Flask(__name__)
-#end point
 
-@app.route('/rossmann/predict', methods = ['POST'])
 
+@app.route('/rossmann/predict', methods=['POST'])
 def rossmann_predict():
     test_json = request.get_json()
+
+    if test_json: #there is data
+        if isinstance(test_json, dict): #unique example
+            test_raw = pd.DataFrame(test_json, index=[0])
+
+        else: #multiple examples
+            test_raw = pd.DataFrame(test_json, columns=test_json[0].keys())
     
-    if test_json: #the is data
-        if isinstance(test_json, dict): #Unique Example 
-            test_raw = pd.DataFrame(test_json, index = [0])
+        #instantiate Rossmann Class
+        pipeline = Rossmann()
+
+        #data cleaning
+        df1 = pipeline.data_cleaning(test_raw)
         
-        else: #multiples examples
-            test_raw = pd.DataFrame( test_json, columns = test_json[0].keys() )
+        #feature engineering
+        df1 = pipeline.feature_engineering(df1)
+        
+        #data preparation
+        df1 = pipeline.data_preparation(df1)
+    
+        #predintion
+        df_response = pipeline.get_prediction(model, test_raw, df1)
+
+
+        return df_response
 
     else:
-        return Response( '{}', status = 200, mimetype = 'application\json')
-    
-    #instantiate Rossmann Class 
-    pipeline = Rossmann()
-    
-    
-    #data_cleaning
-    df1 = pipeline.data_cleaning( test_raw )
-    
-    #feature engineering 
-    df2 = pipeline.feature_engineering( test_raw )
-    
-    #data preparation
-    df3 = pipeline.data_preparation(df2)
-    
-    #prediction
-    df_response = pipeline.get_prediction( model, test_raw, df3)
-    
-    return df_response
-    
-    
-    
-if __name__ ==  '__main__':
+        return Response('{}', status=200, mimetype='application/json')
+
+if __name__ == '__main__':
     port = os.environ.get('PORT', 5000)
-    app.run('0.0.0.0', port=port) 
+    app.run(host = '0.0.0.0', port=port)
